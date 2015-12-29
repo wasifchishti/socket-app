60de177b1758c4c1481b0c7d171edbd74450b4d5
Wasif Chishti <wasifchishti@hotmail.com>
2015-12-29 01:38:02 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,10 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function (message) {
+	console.log('New message:');
+	console.log(message.text);
+});
+
+
--- server.js
+++ server.js
@@ -6,8 +6,18 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: "Welcome to the chat application!"
+	});
 });
 
 http.listen(PORT, function (){
