c3a386a25569dc7ea17ca6ee3aff2f0492da6993
Wasif Chishti <wasifchishti@hotmail.com>
2015-12-29 19:58:56 -0500

    Added @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Send current users to provided sockets
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if(info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on('connection', function (socket) {
 
 	console.log('User connected via socket.io!');
@@ -46,10 +70,14 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = moment().valueOf();
+		if(message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
 
-		// Only send the message to the users who are in the same room as current user
-		io.to(clientInfo[socket.id].room).emit('message', message);
+			// Only send the message to the users who are in the same room as current user
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	// Initial system message
